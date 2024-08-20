e47ec8d078a15b87e8dc8ebc37e4cc9badf5321b
3mbo <timexkdla@gmail.com>
2024-08-20 12:59:25 -0300

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import Header from './components/Header/Header.jsx';
-import Hero from "./components/hero/Hero.jsx";
+import Hero from "./components/Hero/Hero.jsx";
+import './App.css'
+import Companies from './components/Companies/Companies.jsx';
+import Residencies from "./components/Residencies/Residencies.jsx";
+import Value from "./components/Value/Value.jsx";
 function App() {
     return (
         <div className="App">
-            <Header/>
-            <Hero/>
+            <div>
+                <div className="white-gradient"/>
+                <Header/>
+                <Hero/>
+            </div>
+            <Companies/>
+            <Residencies/>
+            <Value/>
         </div>
     );
 }
---
+++ src/components/Companies/Companies.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Companies.css'
+const Companies = () => {
+    return (
+        <section className="c-wrapper">
+            <div className="paddings innerWidth flexCenter c-container">
+                <imp src="./prologis.png" alt=""/>
+                <imp src="./tower.png" alt=""/>
+                <imp src="./equinix.png" alt=""/>
+                <imp src="./realty.png" alt=""/>
+            </div>
+        </section>
+    )
+}
+export default Companies
